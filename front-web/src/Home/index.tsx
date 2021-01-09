f272bc7b525693c512b461fade7caaa92512b01c
unknown <marcos.unitmu@gmail.com>
2021-01-09 18:41:50 -0300

    Add routes and StepsHeader

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
 import Footer from '../Footer';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
@@ -10,23 +11,23 @@ function Home() {
                     <div className="home-actions">
                         <h1 className="home-title">
                             Faça seu pedido <br />
-                       que entregamos <br />
-                       para você!
-                   </h1>
+                            que entregamos <br />
+                            para você!
+                        </h1>
                         <h3 className="home-subtitle">
                             Escolha o seu pedido e em poucos minutos <br />
-                       levaremos na sua porta
-                   </h3>
-                        <a href="#" className="home-btn-order">
+                            levaremos na sua porta
+                        </h3>
+                        <Link to="/orders" className="home-btn-order">
                             FAZER PEDIDO
-                   </a>
+                        </Link>
                     </div>
                     <div className="home-image">
                         <MainImage />
                     </div>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </>
     );
 }
